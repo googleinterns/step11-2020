319d811b0c7a8d6edafd6eaf1fc0bc7e74facc82
Mudit Gupta <muditg317@gmail.com>
2020-06-24 19:43:10 -0400

    add more mentor topics

--- src/main/java/com/google/sps/data/Topic.java
+++ src/main/java/com/google/sps/data/Topic.java
@@ -2,6 +2,13 @@ package com.google.sps.data;
 
 public enum Topic {
   COMPUTER_SCIENCE("Computer Science"),
+  PHYSICS("Physics"),
+  ART("Art"),
+  MEDICINE("Medicine"),
+  MUSIC("Music"),
+  MATH("Math"),
+  LANGUAGE("Language"),
+  BIOLOGY("Biology"),
   OTHER("Other");
 
   private String title;
