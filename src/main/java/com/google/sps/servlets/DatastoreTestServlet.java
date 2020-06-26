54785f9cdc42771f24fc6c3955b5a7b626e5bb15
SylviaZiyuZhang <sylviaziyuz@google.com>
2020-06-26 17:59:03 +0000

    Create file datastore test servlet

---
+++ src/main/java/com/google/sps/servlets/DatastoreTestServlet.java
@@ -0,0 +1,24 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
+import com.google.sps.data.*;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/datastore-test")
+public class DatastoreTestServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+}
