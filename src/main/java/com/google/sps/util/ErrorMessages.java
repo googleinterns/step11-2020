eefc947f15e80658793ed092011e0c542810f9f2
Mudit Gupta <muditg317@gmail.com>
2020-06-26 18:38:07 -0400

    send 500 error instead of reinitializing servlet

---
+++ src/main/java/com/google/sps/util/ErrorMessages.java
@@ -0,0 +1,9 @@
+package com.google.sps.util;
+
+public class ErrorMessages {
+  public static final String TEMPLATES_DIRECTORY_NOT_FOUND = "The templates directory was not found";
+  public static final String TEMPLATE_FILE_NOT_FOUND = "The template was not found: ";
+  public static String templateFileNotFound(tempateURL) {
+    return TEMPLATE_FILE_NOT_FOUND + templateURL;
+  }
+}
