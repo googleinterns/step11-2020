a7013fae1cd1a627baccc9bd7edeb94d96f5c4de
Mudit Gupta <muditg317@gmail.com>
2020-06-26 18:43:16 -0400

    fix compile errors (oops)

--- src/main/java/com/google/sps/util/ErrorMessages.java
+++ src/main/java/com/google/sps/util/ErrorMessages.java
@@ -1,9 +1,11 @@
 package com.google.sps.util;
 
 public class ErrorMessages {
-  public static final String TEMPLATES_DIRECTORY_NOT_FOUND = "The templates directory was not found";
+  public static final String TEMPLATES_DIRECTORY_NOT_FOUND =
+      "The templates directory was not found";
   public static final String TEMPLATE_FILE_NOT_FOUND = "The template was not found: ";
-  public static String templateFileNotFound(tempateURL) {
+
+  public static String templateFileNotFound(String templateURL) {
     return TEMPLATE_FILE_NOT_FOUND + templateURL;
   }
 }
