5aac5950a721e99a0e0826518917b69432d18133
tquintanilla <quintanillathomas00@gmail.com>
2020-06-29 15:42:08 -0400

    refactored constants in ConnectionRequestsServlet

--- src/main/java/com/google/sps/servlets/ConnectionRequestsServlet.java
+++ src/main/java/com/google/sps/servlets/ConnectionRequestsServlet.java
@@ -1,39 +1,82 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.sps.servlets;
 
-import com.google.gson.Gson;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.google.sps.data.Country;
+import com.google.sps.data.EducationLevel;
+import com.google.sps.data.Ethnicity;
+import com.google.sps.data.Gender;
+import com.google.sps.data.Language;
+import com.google.sps.data.MeetingFrequency;
+import com.google.sps.data.MentorType;
+import com.google.sps.data.TimeZoneInfo;
+import com.google.sps.data.Topic;
+import com.google.sps.util.ResourceConstants;
+import com.hubspot.jinjava.Jinjava;
+import com.hubspot.jinjava.JinjavaConfig;
+import com.hubspot.jinjava.loader.FileLocator;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.stream.Collectors;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Collection;
 
 @WebServlet(urlPatterns = URLPatterns.CONNECTION_REQUESTS)
 public class ConnectionRequestServlet extends HttpServlet {
+  private Map<String, Object> context;
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    System.out.println("REQUEST AT: " + request.getServletPath());
-    response.setContentType("text/html;");
-
+  public void init() {
     JinjavaConfig config = new JinjavaConfig();
     Jinjava jinjava = new Jinjava(config);
     try {
       jinjava.setResourceLocator(
           new FileLocator(
               new File(this.getClass().getResource(ResourceConstants.TEMPLATES).toURI())));
-    } catch (URISyntaxException e) {
+    } catch (URISyntaxException | FileNotFoundException e) {
       System.err.println("templates dir not found!");
     }
 
-    Map<String, Object> context = new HashMap<>();
+    context =  new HashMap<>();
     context.put("url", "/");
-    context.put("requests",  DummyDataAccess.getIncomingRequests(DummyDataAccess.getCurrentUser()));
-    String template =
-        Resources.toString(
-            this.getClass().getResource(ResourceConstants.TEMPLATE_ABOUT), Charsets.UTF_8);
-    String renderedTemplate = jinjava.render(template, context);
 
-    response.getWriter().println(renderedTemplate);
+    try {
+      String template =
+          Resources.toString(
+              this.getClass().getResource(ResourceConstants.TEMPLATE_CONNECTION_REQUESTS), Charsets.UTF_8);
+      staticResponse = jinjava.render(template, context);
+    } catch (IOException e) {
+      System.err.println("template"  + ResourceConstants.TEMPLATE_CONNECTION_REQUESTS +  " not found");
     }
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    System.out.println("REQUEST AT: " + request.getServletPath());
+    response.setContentType("text/html;");
+    context.put("connection-requests",  DummyDataAccess.getIncomingRequests(DummyDataAccess.getCurrentUser()));
+
+    response.getWriter().println(staticResponse);
+  }
 }
--- src/main/java/com/google/sps/util/ResourceConstants.java
+++ src/main/java/com/google/sps/util/ResourceConstants.java
@@ -8,4 +8,5 @@ public class ResourceConstants {
   public static final String TEMPLATE_QUESTIONNAIRE = "/templates/questionnaire.html";
   public static final String TEMPLATE_LANDING = "/templates/landing.html";
   public static final String TEMPLATE_PROFILE = "/templates/profile.html";
+  public static final String TEMPLATE_CONNECTION_REQUESTS = "/templates/connection-requests.html"
 }
