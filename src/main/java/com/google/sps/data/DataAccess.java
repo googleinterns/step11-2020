ca6bd54b18f4f852253d10fb2198ae05c0b82a33
tquintanilla <quintanillathomas00@gmail.com>
2020-06-29 15:10:49 -0400

    placed getCurrentUser with similar methods

--- src/main/java/com/google/sps/data/DataAccess.java
+++ src/main/java/com/google/sps/data/DataAccess.java
@@ -9,6 +9,8 @@ interface DataAccess {
 
   UserAccount getUser(Key datastoreKey);
 
+  UserAccount getCurrentUser();
+
   Collection<Mentor> getRelatedMentors(Mentee mentee);
 
   Collection<Mentee> getRelatedMentees(Mentor mentor);
@@ -31,5 +33,4 @@ interface DataAccess {
   // delete request object
   void denyRequest(MentorshipRequest request);
 
-  UserAccount getCurrentUser();
 }
