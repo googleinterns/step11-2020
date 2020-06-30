30945bc194023483b9bcb2ae1805ae8011784187
Mudit Gupta <muditg317@gmail.com>
2020-06-30 11:43:28 -0400

    setup shortlist page

---
+++ src/main/java/com/google/sps/servlets/ShortlistServlet.java
@@ -0,0 +1,68 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.google.sps.data.DummyDataAccess;
+import com.google.sps.data.Mentor;
+import com.google.sps.util.ErrorMessages;
+import com.google.sps.util.ResourceConstants;
+import com.google.sps.util.URLPatterns;
+import com.hubspot.jinjava.Jinjava;
+import com.hubspot.jinjava.JinjavaConfig;
+import com.hubspot.jinjava.loader.FileLocator;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = URLPatterns.SHORTLIST)
+public class ShortlistServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
+
+    JinjavaConfig config = new JinjavaConfig();
+    Jinjava jinjava = new Jinjava(config);
+    try {
+      jinjava.setResourceLocator(
+          new FileLocator(
+              new File(this.getClass().getResource(ResourceConstants.TEMPLATES).toURI())));
+    } catch (URISyntaxException e) {
+      System.err.println(ErrorMessages.TEMPLATES_DIRECTORY_NOT_FOUND);
+    }
+
+    Map<String, Object> context = new HashMap<>();
+    context.put(URLPatterns.URL, URLPatterns.SHORTLIST);
+    Collection<Mentor> relatedMentors = new DummyDataAccess().getRelatedMentors(null);
+    context.put("mentors", relatedMentors);
+
+    String template =
+        Resources.toString(
+            this.getClass().getResource(ResourceConstants.TEMPLATE_SHORTLIST), Charsets.UTF_8);
+
+    String renderedTemplate = jinjava.render(template, context);
+
+    response.getWriter().println(renderedTemplate);
+  }
+}
--- src/main/java/com/google/sps/util/URLPatterns.java
+++ src/main/java/com/google/sps/util/URLPatterns.java
@@ -11,4 +11,5 @@ public class URLPatterns {
   public static final String MAIN = "/main";
   public static final String PROFILE = "/profile";
   public static final String QUESTIONNAIRE = "/questionnaire";
+  public static final String SHORTLIST = "/shortlist";
 }
