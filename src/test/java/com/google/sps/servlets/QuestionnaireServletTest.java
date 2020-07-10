b204ea03ec3127150b4d6d00f455149f5368ae0a
tquintanilla <quintanillathomas00@gmail.com>
2020-07-10 16:43:59 -0400

    basic structure and dependencies for servlet tests

--- src/test/java/com/google/sps/servlets/QuestionnaireServletTest.java
+++ src/test/java/com/google/sps/servlets/QuestionnaireServletTest.java
@@ -1,20 +1,16 @@
 package com.google.sps.tests;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.appengine.tools.development.testing.LocalUserServiceTestConfig;
+import com.google.sps.data.DatastoreAccess;
 import com.google.sps.servlets.QuestionnaireServlet;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.junit.Assert;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -22,47 +18,21 @@ import org.mockito.MockitoAnnotations;
 public final class QuestionnaireServletTest {
   @Mock private HttpServletRequest request;
   @Mock private HttpServletResponse response;
-  @Mock private HttpServlet servlet;
+  @Mock private DatastoreAccess dataAccess;
+  @InjectMocks private QuestionnaireServlet servlet;
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(
+              new LocalUserServiceTestConfig().setOAuthUserId("foo").setOAuthEmail("foo@gmail.com"))
+          .setEnvIsLoggedIn(true);
 
   @Before
   public void setUp() throws Exception {
     MockitoAnnotations.initMocks(this);
-    request = mock(HttpServletRequest.class);
-    response = mock(HttpServletResponse.class);
+    helper.setUp();
   }
 
-  // @Test
-  // public void
-
-  @Test
-  public void correctNameInText() throws Exception {
-    when(request.getParameter("name")).thenReturn("jake");
-
-    StringWriter stringWriter = new StringWriter();
-    PrintWriter writer = new PrintWriter(stringWriter);
-
-    when(response.getWriter()).thenReturn(writer);
-
-    new QuestionnaireServlet().doPost(request, response);
-
-    verify(request).getParameter("name");
-    writer.flush();
-    Assert.assertTrue(stringWriter.toString().contains("jake"));
-  }
-
-  @Test
-  public void useDefaultValueWhenNoneProvidedForString() throws Exception {
-    when(request.getParameter("name")).thenReturn("");
-
-    StringWriter stringWriter = new StringWriter();
-    PrintWriter writer = new PrintWriter(stringWriter);
-
-    when(response.getWriter()).thenReturn(writer);
-
-    new QuestionnaireServlet().doPost(request, response);
-
-    verify(request).getParameter("name");
-    writer.flush();
-    Assert.assertTrue(stringWriter.toString().contains("John Doe"));
+  @After
+  public void tearDown() {
+    helper.tearDown();
   }
 }
