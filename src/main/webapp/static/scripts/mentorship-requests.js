46b6b96d6d878ae9226164b9001014d7e1721e33
Mudit Gupta <muditg317@gmail.com>
2020-07-24 14:29:15 -0400

    improve and beautify mentorship-requests page

--- src/main/webapp/static/scripts/mentorship-requests.js
+++ src/main/webapp/static/scripts/mentorship-requests.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 const menteeCardContainer = document.getElementById("mentee-card-container");
-Array.prototype.forEach.call(document.querySelectorAll(".mentee-card"), (menteeCard) => {
+document.querySelectorAll(".mentee-card").forEach(menteeCard => {
   const acceptButton = menteeCard.querySelector("button#yes");
   const denyButton = menteeCard.querySelector("button#no");
   acceptButton.addEventListener("click", async (event) => {
@@ -37,7 +37,6 @@ const sendRequestDecision = async (requestID, choice) => {
       method: "POST",
       body: new URLSearchParams({requestID, choice})
     });
-  let text = await response.text();
-  let data = JSON.parse(text);
+  let data = await response.json();
   return data.success;
 }
